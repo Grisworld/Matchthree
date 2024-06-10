fe2f0b11c8f8974925cd093d82bc2786ab91fe54
Grisworld <potemkin11>
2024-06-10 19:09:07 +0300

    Fixed gizmo selecting algorithm problem and anaylized everything learn more!

--- Assets/Scripts/Components/GridManager.cs
+++ Assets/Scripts/Components/GridManager.cs
@@ -1,34 +1,32 @@
-using System;
- using System.Collections.Generic;
- using System.Reflection;
- using Events;
-using Extensions.System;
-using Extensions.Unity;
+using System.Collections.Generic;
+using Events;
 using Sirenix.OdinInspector;
 using Sirenix.Serialization;
-using Sirenix.Utilities;
-using UnityEditor;
 using UnityEngine;
 using Zenject;
-using Random = UnityEngine.Random;
 
 namespace Components
 {
-    public class GridManager : SerializedMonoBehaviour
+    public partial class GridManager : SerializedMonoBehaviour
     {
         [Inject] private InputEvents InputEvents{get;set;}
-        [SerializeField] private Camera Camera;
-        [BoxGroup(Order = 999)][TableMatrix(SquareCells = true)/*(DrawElementMethod = nameof(DrawTile))*/,OdinSerialize]
-        private Tile[,] _grid;
+        [Inject] private GridEvents GridEvents{get;set;}
+        [BoxGroup(Order = 999), TableMatrix(SquareCells = true, DrawElementMethod = nameof(DrawTile)),OdinSerialize] private Tile[,] _grid;
         [SerializeField] private List<GameObject> _tilePrefabs;
         private int _gridSizeX;
         private int _gridSizeY;
         [SerializeField] private List<int> _prefabIds;
-        
+        [SerializeField] private Bounds _gridBounds;
         private Tile _selectedTile;
         private Vector3 _mouseDownPos;
         private Vector3 _mouseUpPos;
-        
+        private List<Tile> _currMatchesDebug;
+
+        private void Start()
+        {
+            GridEvents.GridLoaded?.Invoke(_gridBounds);
+        }
+
         private void OnEnable()
         {
             RegisterEvents();
@@ -39,214 +37,71 @@ namespace Components
             UnRegisterEvents();
         }
 
-        
-        static MethodInfo _clearConsoleMethod;
-        static MethodInfo clearConsoleMethod {
-            get {
-                if (_clearConsoleMethod == null) {
-                    Assembly assembly = Assembly.GetAssembly (typeof(SceneView));
-                    Type logEntries = assembly.GetType ("UnityEditor.LogEntries");
-                    _clearConsoleMethod = logEntries.GetMethod ("Clear");
-                }
-                return _clearConsoleMethod;
-            }
-        }
-        private Tile DrawTile(Rect rect, Tile tile)
-        {
-            if (tile != null)
-            {
-                Sprite spriteTile = tile.GetComponent<SpriteRenderer>().sprite;
-                Rect textureRect = rect;
-                textureRect.Padding(3);
-                UnityEditor.EditorGUI.DrawPreviewTexture(textureRect, spriteTile.texture);
-            }
-
-            return tile;
-        }
-
-        [Button]
-        private void CreateGrid(int sizeX, int sizeY)
-        {
-            _gridSizeX = sizeX;
-            _gridSizeY = sizeY;
-            Camera.transform.SetPositionAndRotation(new Vector3(_gridSizeX / 2, _gridSizeY / 2, _gridSizeY * (-1f)), Quaternion.identity);
-            for(int id = 0; id < _tilePrefabs.Count; id ++) _prefabIds.Add(id);
-            if (_grid != null)
-            {
-                foreach (Tile o in _grid)
-                {
-                    if (o != null)
-                        DestroyImmediate(o.gameObject);
-                }
-            }
-
-            _grid = new Tile[_gridSizeX, _gridSizeY];
-
-            for (int x = 0; x < _gridSizeX; x++)
-            for (int y = 0; y < _gridSizeY; y++)
-            {
-                Vector2Int coord = new(x, _gridSizeY - y - 1);
-                Vector3 pos = new(coord.x, coord.y, 0f);
-                while (true)
-                {
-                    List<int> spawnableIds = new(_prefabIds);
-                    int randomId = spawnableIds.Random();
-                    int randomIndex = Random.Range(0, _tilePrefabs.Count);
-                    GameObject tilePrefabRandom = _tilePrefabs[randomIndex];
-                    GameObject tileNew = Instantiate(tilePrefabRandom, pos, Quaternion.identity);
-                    Tile tile = tileNew.GetComponent<Tile>();
-                    //Debug.Log(tile.ID);
-                    //Debug.Log(x + "  " + (_gridSizeY - y - 1));
-                    int tileID = tile.ID;
-                    bool avail = true;
-                    if (x >= 2 && _grid[x - 1, y].ID == tileID && _grid[x - 2, y].ID == tileID)
-                        avail = false;
-                    if (y >= 2 && _grid[x, y - 1].ID == tileID && _grid[x, y - 2].ID == tileID)
-                        avail = false;
-                    if (avail)
-                    {
-                        CreateAndQuit(tile, coord, x, y);
-                        break;
-                    }
-                    DestroyImmediate(tileNew);
-                }
-            }
-            clearConsoleMethod.Invoke (new object (), null);
-            Debug.Log("Any Move? "+ ControlMoves(_gridSizeX, _gridSizeY));
-            FindAllPossibleMoves(_gridSizeX, _gridSizeY);
-            
-        }
-
-        private void FindAllPossibleMoves(int gridSizeX, int gridSizeY)
-        {
-            List<Tuple<int,int,String>> positions = new List<Tuple<int,int,String>>();
-            for (int x = 0; x < gridSizeX; x++)
-            for (int y = 0; y < gridSizeY; y++)
-            {
-                if (y < gridSizeY - 3 && _grid[x, y + 2].ID == _grid[x, y].ID && _grid[x, y + 3].ID == _grid[x, y].ID)
-                    positions.Add(new Tuple<int, int,String>(x,y,"YPos"));
-                if (y >= 3 && _grid[x, y - 2].ID == _grid[x, y].ID && _grid[x, y - 3].ID == _grid[x, y].ID)
-                    positions.Add(new Tuple<int, int,String>(x,y,"YPos"));
-                if (y < gridSizeY - 1 && x >= 2 && _grid[x - 1, y + 1].ID == _grid[x, y].ID && _grid[x - 2, y + 1].ID == _grid[x, y].ID)
-                    positions.Add(new Tuple<int, int,String>(x,y,"YPos"));
-                if (y < gridSizeY - 1 && x < gridSizeX - 2 && _grid[x + 1, y + 1].ID == _grid[x, y].ID && _grid[x + 2, y + 1].ID == _grid[x, y].ID)
-                    positions.Add(new Tuple<int, int,String>(x,y,"YPos"));
-                if (y >= 1 && x >= 2 && _grid[x - 1, y - 1].ID == _grid[x, y].ID && _grid[x - 2, y - 1].ID == _grid[x, y].ID)
-                    positions.Add(new Tuple<int, int,String>(x,y,"YPos"));
-                if (y >= 1 && x < gridSizeX - 2 && _grid[x + 1, y - 1].ID == _grid[x, y].ID && _grid[x + 2, y - 1].ID == _grid[x, y].ID)
-                    positions.Add(new Tuple<int, int,String>(x,y,"YPos"));
-                if (y < gridSizeY - 1 && x < gridSizeX - 1 && x >= 1 && _grid[x + 1, y + 1].ID == _grid[x, y].ID && _grid[x - 1, y + 1].ID == _grid[x, y].ID)
-                    positions.Add(new Tuple<int, int,String>(x,y,"YPos"));
-                if (y >= 1 && x < gridSizeX - 1 && x >= 1 && _grid[x + 1, y - 1].ID == _grid[x, y].ID && _grid[x - 1, y - 1].ID == _grid[x, y].ID)
-                    positions.Add(new Tuple<int, int,String>(x,y,"YPos"));
-                
-                if (x < gridSizeX - 3 && _grid[x + 2, y].ID == _grid[x, y].ID && _grid[x + 3, y].ID == _grid[x, y].ID)
-                    positions.Add(new Tuple<int, int,String>(x,y,"XPos"));
-                if (x >= 3 && _grid[x - 2, y].ID == _grid[x, y].ID && _grid[x - 3, y].ID == _grid[x, y].ID)
-                    positions.Add(new Tuple<int, int,String>(x,y,"XPos"));
-                if (x >= 1 && y < gridSizeY - 2 && _grid[x - 1, y + 1].ID == _grid[x, y].ID && _grid[x - 1, y + 2].ID == _grid[x, y].ID)
-                    positions.Add(new Tuple<int, int,String>(x,y,"XPos"));
-                if (x >= 1 && y >= 2 && _grid[x - 1, y - 1].ID == _grid[x, y].ID && _grid[x - 1, y - 2].ID == _grid[x, y].ID)
-                    positions.Add(new Tuple<int, int,String>(x,y,"XPos"));
-                if (x < gridSizeX - 1 && y < gridSizeY - 2 && _grid[x + 1, y + 1].ID == _grid[x, y].ID && _grid[x + 1, y + 2].ID == _grid[x, y].ID)
-                    positions.Add(new Tuple<int, int,String>(x,y,"XPos"));
-                if (x < gridSizeX - 1 && y >= 2 && _grid[x + 1, y - 1].ID == _grid[x, y].ID && _grid[x + 1, y - 2].ID == _grid[x, y].ID)
-                    positions.Add(new Tuple<int, int,String>(x,y,"XPos"));
-                if (x < gridSizeX - 1 && y < gridSizeY - 1 && y >= 1 && _grid[x + 1, y + 1].ID == _grid[x, y].ID && _grid[x + 1, y - 1].ID == _grid[x, y].ID)
-                    positions.Add(new Tuple<int, int,String>(x,y,"XPos"));
-                if (x >= 1 && y < gridSizeY - 1 && y >= 1 && _grid[x - 1, y + 1].ID == _grid[x, y].ID && _grid[x - 1, y - 1].ID == _grid[x, y].ID)
-                    positions.Add(new Tuple<int, int,String>(x,y,"XPos"));
-                
-            }
-
-            int count = 0;
-            int temp = -1;
-            int temp2 = -1;
-            foreach (var tuples in positions)
-            {
-                count = temp == tuples.Item1 && temp2 == tuples.Item2 ? count + 1 : 0;
-                Debug.Log("Positions= "+ tuples.Item1 + " "+tuples.Item2 + " " +tuples.Item3);
-                temp = tuples.Item1;
-                temp2 = tuples.Item2;
-                if(count == 6)
-                    Debug.LogError("Max "+ tuples.Item1+ " "+tuples.Item2 + " " +tuples.Item3);
-            }
-        }
-
-        private bool ControlMoves(int gridSizeX, int gridSizeY)
-        {
-            
-            for (int x = 0; x < gridSizeX; x++)
-            for (int y = 0; y < gridSizeY; y++)
-            {
-                //Y CASES
-                if (y < gridSizeY - 3 && _grid[x, y + 2].ID == _grid[x, y].ID && _grid[x, y + 3].ID == _grid[x, y].ID)
-                    return true;
-                if (y >= 3 && _grid[x, y - 2].ID == _grid[x, y].ID && _grid[x, y - 3].ID == _grid[x, y].ID)
-                    return true;
-                if (y < gridSizeY - 1 && x >= 2 && _grid[x - 1, y + 1].ID == _grid[x, y].ID && _grid[x - 2, y + 1].ID == _grid[x, y].ID)
-                    return true;
-                if (y < gridSizeY - 1 && x < gridSizeX - 2 && _grid[x + 1, y + 1].ID == _grid[x, y].ID && _grid[x + 2, y + 1].ID == _grid[x, y].ID)
-                    return true;
-                if (y >= 1 && x >= 2 && _grid[x - 1, y - 1].ID == _grid[x, y].ID && _grid[x - 2, y - 1].ID == _grid[x, y].ID)
-                    return true;
-                if (y >= 1 && x < gridSizeX - 2 && _grid[x + 1, y - 1].ID == _grid[x, y].ID && _grid[x + 2, y - 1].ID == _grid[x, y].ID)
-                    return true;
-                if (y < gridSizeY - 1 && x < gridSizeX - 1 && x >= 1 && _grid[x + 1, y + 1].ID == _grid[x, y].ID && _grid[x - 1, y + 1].ID == _grid[x, y].ID)
-                    return true;
-                if (y >= 1 && x < gridSizeX - 1 && x >= 1 && _grid[x + 1, y - 1].ID == _grid[x, y].ID && _grid[x - 1, y - 1].ID == _grid[x, y].ID)
-                    return true;
-                
-                //X CASES
-                if (x < gridSizeX - 3 && _grid[x + 2, y].ID == _grid[x, y].ID && _grid[x + 3, y].ID == _grid[x, y].ID)
-                    return true;
-                if (x >= 3 && _grid[x - 2, y].ID == _grid[x, y].ID && _grid[x - 3, y].ID == _grid[x, y].ID)
-                    return true;
-                if (x >= 1 && y < gridSizeY - 2 && _grid[x - 1, y + 1].ID == _grid[x, y].ID && _grid[x - 1, y + 2].ID == _grid[x, y].ID)
-                    return true;
-                if (x >= 1 && y >= 2 && _grid[x - 1, y - 1].ID == _grid[x, y].ID && _grid[x - 1, y - 2].ID == _grid[x, y].ID)
-                    return true;
-                if (x < gridSizeX - 1 && y < gridSizeY - 2 && _grid[x + 1, y + 1].ID == _grid[x, y].ID && _grid[x + 1, y + 2].ID == _grid[x, y].ID)
-                    return true;
-                if (x < gridSizeX - 1 && y >= 2 && _grid[x + 1, y - 1].ID == _grid[x, y].ID && _grid[x + 1, y - 2].ID == _grid[x, y].ID)
-                    return true;
-                if (x < gridSizeX - 1 && y < gridSizeY - 1 && y >= 1 && _grid[x + 1, y + 1].ID == _grid[x, y].ID && _grid[x + 1, y - 1].ID == _grid[x, y].ID)
-                    return true;
-                if (x >= 1 && y < gridSizeY - 1 && y >= 1 && _grid[x - 1, y + 1].ID == _grid[x, y].ID && _grid[x - 1, y - 1].ID == _grid[x, y].ID)
-                    return true;
-                
-            }
-
-            return false;
-        }
-
-        private void CreateAndQuit(Tile tile, Vector2Int coord, int x, int y)
-        {
-            tile.Construct(coord);
-            _grid[x, y] = tile;
-        }
         private void RegisterEvents()
         {
             InputEvents.MouseDownGrid += OnMouseDownGrid;
             InputEvents.MouseUpGrid += OnMouseUpGrid;
         }
 
-        private void OnMouseDownGrid(Tile arg0, Vector3 arg1)
+        private void OnMouseDownGrid(Tile clickedTile, Vector3 dirVector)
         {
-            _selectedTile = arg0;
-            _mouseDownPos = arg1;
-            EDebug.Method();
+            _selectedTile = clickedTile;
+            _mouseDownPos = dirVector;
+        }
 
+        private bool CanMove(Tile clickedTile, Vector3 inputVect, out List<Tile> matches)
+        {
+            matches = null;
+            
+            Vector2Int tileMoveCoord = clickedTile.Coords + GridF.GetGridDirVector(inputVect);
+
+            if(_grid.IsInsideGrid(tileMoveCoord) == false) return false;
+
+            return HasMatch(clickedTile, tileMoveCoord, out matches);
+        }
+
+        private bool HasMatch(Tile fromTile, Vector2Int tileMoveCoord, out List<Tile> matches)
+        {
+            bool hasMatches = false;
+            matches = new List<Tile>();   
+            Tile toTile = _grid.Get(tileMoveCoord);
+            _grid.Switch(fromTile, toTile);
+
+            
+            matches.AddRange(_grid.GetMatchesY(fromTile));
+            matches.AddRange(_grid.GetMatchesX(fromTile));
+            
+
+            if(matches.Count > 2)
+            {
+                hasMatches = true;
+            }
+            
+            _grid.Switch(toTile, fromTile);
+            return hasMatches;
+        }
+
+        [Button]
+        private void TestGridDir(Vector2 input)
+        {
+            Debug.LogWarning(GridF.GetGridDir(input));
         }
 
         private void OnMouseUpGrid(Vector3 arg0)
         {
             _mouseUpPos = arg0;
 
+            Vector3 dirVector = arg0 - _mouseDownPos;
+
             if(_selectedTile)
             {
-                EDebug.Method();
-    
+                bool canMove = CanMove(_selectedTile, dirVector, out List<Tile> matches);
+                Debug.LogWarning($"{canMove} canMove, {(matches != null ? matches.Count : 0)} matches.Count");
+
+                if(! canMove) return;
+
+                _currMatchesDebug = matches;
+                
                 Debug.DrawLine(_mouseDownPos, _mouseUpPos, Color.blue, 2f);
             }
         }
--- Assets/Scripts/GridF.cs
+++ Assets/Scripts/GridF.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using Components;
+using Extensions.System;
 using UnityEngine;
 
 public static class GridF
@@ -86,139 +88,220 @@ public static class GridF
             if(lastIDCounter == MatchOffset) results.Remove(lastPrefabID);
         }
     }
-
-    public static bool HasMatchesRight(this Tile[,] grid, Vector2Int abstractCoord, int prefabId)
-    {
-        if(grid.IsInsideGrid(abstractCoord))
-        {
-            
-        }
-
-        return default;
-    }
     
-    private static bool HasMatchRight(this Tile[,] grid, Vector2Int coord, int prefabId)
-    {
-        int rightMax = coord.x + MatchOffset;
-        rightMax = ClampInsideGrid(rightMax, grid.GetLength(0));
-
-        int matchCounter = 0;
-        
-        for(int x = coord.x; x <= rightMax; x ++)
-        {
-            if(grid[x, coord.y].ID == prefabId)
-            {
-                matchCounter ++;
-            }
-            else
-            {
-                matchCounter = 0;
-            }
-
-            if(matchCounter == 3)
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
+    public static List<Tile> GetMatchesX
+    (this Tile[,] thisGrid, Tile tile)
+        => GetMatchesX(thisGrid, tile.Coords, tile.ID);
     
-    private static bool HasMatchLeft(this Tile[,] grid, Vector2Int coord, int prefabId)
+    public static List<Tile> GetMatchesX(this Tile[,] grid, Vector2Int coord, int prefabId)
     {
-        int leftMax = coord.x - MatchOffset;
-        leftMax = ClampInsideGrid(leftMax, grid.GetLength(0));
+        Tile thisTile = grid.Get(coord);
 
-        int matchCounter = 0;
-        
-        for(int x = leftMax; x <= coord.x; x ++)
+        List<Tile> matches = new();
+        int lastID = -1;
+        for(int x = 0; x < grid.GetLength(0); x ++)
         {
-            if(grid[x, coord.y].ID == prefabId)
+            Tile currTile = grid[x, coord.y];
+
+            int difference = Math.Abs(currTile.Coords.x - thisTile.Coords.x);
+            if(currTile.ID == prefabId && difference <= MatchOffset)
             {
-                matchCounter ++;
+                Debug.Log("EnteredX?  "+currTile.Coords);
+                if(lastID == -1 || currTile.ID == lastID)
+                    matches.Add(currTile);
+                lastID = currTile.ID;
+
+            }
+            else if (matches.Count < MatchOffset + 1)
+            {
+                matches.Clear();
             }
             else
             {
-                matchCounter = 0;
+                lastID = currTile.ID;
             }
 
-            if(matchCounter == 3)
-            {
-                return true;
-            }
         }
-
-        return false;
+        ControlCountThree(matches);
+        return matches;
     }
-    private static bool HasMatchTop(this Tile[,] grid, Vector2Int coord, int prefabId)
-    {
-        int topMax = coord.y + MatchOffset;
-        topMax = ClampInsideGrid(topMax, grid.GetLength(1));
 
-        int matchCounter = 0;
-        
-        for(int y = coord.y; y <= topMax; y ++)
+    public static List<Tile> GetMatchesY
+    (this Tile[,] thisGrid, Tile tile)
+        => GetMatchesY(thisGrid, tile.Coords, tile.ID);
+    public static List<Tile> GetMatchesY(this Tile[,] grid, Vector2Int coord, int prefabId)
+    {
+        Tile thisTile = grid.Get(coord);
+
+        List<Tile> matches = new();
+        int lastID = -1;
+        for(int y = 0; y < grid.GetLength(1); y ++)
         {
-            if(grid[coord.x, y].ID == prefabId)
+            Tile currTile = grid[coord.x, y];
+
+            int difference = Math.Abs(currTile.Coords.y - thisTile.Coords.y);
+            if(currTile.ID == prefabId && difference <= MatchOffset)
             {
-                matchCounter ++;
+                Debug.Log("EnteredY?  "+currTile.Coords);
+                if(lastID == -1 || currTile.ID == lastID)
+                    matches.Add(currTile);
+                lastID = currTile.ID;
+
+            }
+            else if (matches.Count < MatchOffset + 1)
+            {
+                matches.Clear();
             }
             else
             {
-                matchCounter = 0;
-            }
 
-            if(matchCounter == 3)
-            {
-                return true;
+                lastID = currTile.ID;
             }
         }
-
-        return false;
+        ControlCountThree(matches);
+        return matches;
     }
-    
-    private static bool HasMatchBot(this Tile[,] grid, Vector2Int coord, int prefabId)
+
+    private static void ControlCountThree(List<Tile> matches)
     {
-        int botMax = coord.y - MatchOffset;
-        botMax = ClampInsideGrid(botMax, grid.GetLength(1));
-
-        int matchCounter = 0;
-        
-        for(int y = botMax; y <= coord.y; y ++)
-        {
-            if(grid[coord.x, y].ID == prefabId)
-            {
-                matchCounter ++;
-            }
-            else
-            {
-                matchCounter = 0;
-            }
-
-            if(matchCounter == 3)
-            {
-                return true;
-            }
-        }
-
-        return false;
+        if (matches.Count < MatchOffset + 1)
+            matches.Clear();
     }
+
     private static int ClampInsideGrid
     (int value, int gridSize)
     {
         return Mathf.Clamp(value, 0, gridSize - 1);
     }
 
-    private static bool IsInsideGrid(this Tile[,] grid, int axis, int axisIndex)
+    public static bool IsInsideGrid(this Tile[,] grid, int axis, int axisIndex)
     {
         int min = 0;
         int max = grid.GetLength(axisIndex);
         
-        return axis >= 0 && axis < max;
+        return axis >= min && axis < max;
     }
     
-    private static bool IsInsideGrid(this Tile[,] grid, Vector2Int coord)
+    public static bool IsInsideGrid(this Tile[,] grid, Vector2Int coord)
     {
         return grid.IsInsideGrid(coord.x, 0) && grid.IsInsideGrid(coord.y, 1);
     }
+
+    public static GridDir GetGridDir(Vector3 input)
+    {
+        int maxAxis = 0;
+        float maxAxisSign = input[0].Sign();
+        float lastAxisLengthAbs = input[0].Abs();
+        
+        for(int axisIndex = 0; axisIndex < 3; axisIndex ++)
+        {
+            float thisAxisLength = input[axisIndex];
+            float thisAxisLengthAbs = thisAxisLength.Abs();
+
+            if(thisAxisLengthAbs > lastAxisLengthAbs)
+            {
+                lastAxisLengthAbs = thisAxisLengthAbs;
+                maxAxis = axisIndex;
+                maxAxisSign = thisAxisLength.Sign();
+            }
+        }
+
+        return GetGridDir((maxAxis + 1) * maxAxisSign.CeilToInt());
+    }
+    
+    public static Vector2Int GetGridDirVector(Vector3 input)
+    {
+        int maxAxis = 0;
+        float maxAxisSign = input[0].Sign();
+        float lastAxisLengthAbs = input[0].Abs();
+        
+        for(int axisIndex = 0; axisIndex < 3; axisIndex ++)
+        {
+            float thisAxisLength = input[axisIndex];
+            float thisAxisLengthAbs = thisAxisLength.Abs();
+
+            if(thisAxisLengthAbs > lastAxisLengthAbs)
+            {
+                lastAxisLengthAbs = thisAxisLengthAbs;
+                maxAxis = axisIndex;
+                maxAxisSign = thisAxisLength.Sign();
+            }
+        }
+
+        return GetGridDir((maxAxis + 1) * maxAxisSign.CeilToInt()).ToVector();
+    }
+    
+    /// <summary>
+    /// Convert non-zero axis index with sign.
+    /// </summary>
+    /// <param name="axisSignIndex">Should not start from zero.</param>
+    /// <returns>GridDir</returns>
+    public static GridDir GetGridDir(int axisSignIndex)
+    {
+        return axisSignIndex switch
+        {
+            1 => GridDir.Right,
+            2 => GridDir.Up,
+            -1 => GridDir.Left,
+            -2 => GridDir.Down,
+            _ => GridDir.Null
+        };
+    }
+
+    public static Vector2Int ToVector(this GridDir thisGridDir)
+    {
+        return thisGridDir switch
+        {
+            GridDir.Null => Vector2Int.zero,
+            GridDir.Left => Vector2Int.left,
+            GridDir.Right => Vector2Int.right,
+            GridDir.Up => Vector2Int.up,
+            GridDir.Down => Vector2Int.down,
+            _ => throw new ArgumentOutOfRangeException(nameof(thisGridDir), thisGridDir, null)
+        };
+    }
+
+    public static Tile Get(this Tile[,] thisGrid, Vector2Int coord)
+    {
+        return thisGrid[coord.x, coord.y];
+    }
+    
+    public static Tile Set(this Tile[,] thisGrid, Tile tileToSet, Vector2Int coord)
+    {
+        Tile tileAtCoord = thisGrid.Get(coord);
+
+        thisGrid[coord.x, coord.y] = tileToSet;
+        
+        ICoordSet coordSet = tileToSet;
+
+        coordSet.SetCoord(coord);
+        
+        return tileAtCoord;
+    }
+
+    public static void Switch(this Tile[,] thisGrid, Tile fromTile, Vector2Int toCoords)
+    {
+        Vector2Int fromCoords = fromTile.Coords;
+        
+        Tile toTile = thisGrid.Set(fromTile, toCoords);
+        thisGrid.Set(toTile, fromCoords);
+    }
+    
+    public static void Switch(this Tile[,] thisGrid, Tile fromTile, Tile toTile)
+    {
+        Vector2Int fromCoords = fromTile.Coords;
+        Vector2Int toCoords = toTile.Coords;
+        
+        thisGrid.Set(fromTile, toCoords);
+        thisGrid.Set(toTile, fromCoords);
+    }
+}
+
+public enum GridDir
+{
+    Null,
+    Left,
+    Right,
+    Up,
+    Down
 }
